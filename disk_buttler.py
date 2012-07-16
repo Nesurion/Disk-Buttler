e0b36681d5631ae918763b1ae4bef8ea38ea7a8f
Nesurion <maik.schwan@gmail.com>
2012-07-16 21:50:33 +0200

    filterd files from list

--- disk_buttler.py
+++ disk_buttler.py
@@ -8,3 +8,12 @@ dst = os.path.abspath(sys.argv[2])
 
 #create a list containing src subfolder foldernames
 src_content = os.listdir(src)
+print src_content
+
+only_dir_names = []
+for src_element in src_content:
+	fullpath = os.path.join(src, src_element)
+	if os.path.isdir(fullpath):
+		only_dir_names.append(src_element)
+
+print only_dir_names
