7873a497b0e67a4c64c3d7389f0a73ae56b4a8d9
Nesurion <maik.schwan@gmail.com>
2012-07-16 21:25:20 +0200

    restart! refs #1

--- disk_buttler.py
+++ disk_buttler.py
@@ -8,11 +8,3 @@ dst = os.path.abspath(sys.argv[2])
 
 #create a list containing src subfolder foldernames
 src_content = os.listdir(src)
-
-#deleting the filenames from listdir
-def f(src_element):
-	return os.path.join(src, src_element)
-
-src_with_prefix = map(f, src_content)
-dirs_only = filter(os.path.isdir, src_with_prefix) 
-print dirs_only
