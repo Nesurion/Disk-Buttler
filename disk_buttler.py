ce6e0ac8bf8f525965eac6de4680f6d6732f341a
Nesurion <maik.schwan@gmail.com>
2012-07-16 22:09:35 +0200

    with comments this time ;) refs #1

--- disk_buttler.py
+++ disk_buttler.py
@@ -8,12 +8,18 @@ dst = os.path.abspath(sys.argv[2])
 
 #create a list containing src subfolder foldernames
 src_content = os.listdir(src)
-print src_content
 
 only_dir_names = []
 for src_element in src_content:
+	# combine the user src path with subfolder to a full path,
+	# so path.isdir can check if it is dir or not
 	fullpath = os.path.join(src, src_element)
+	#check if path end with dir or file and write dirs into "only_dir_names"
 	if os.path.isdir(fullpath):
 		only_dir_names.append(src_element)
 
 print only_dir_names
+
+#next up:
+#generate folder in dst path based on only_dir_names
+
